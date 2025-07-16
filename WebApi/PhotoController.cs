72fd569dee323eab7256a0c59725c8a1088324df
Mateusz <PRS-M@users.noreply.github.com>
2025-07-16 22:16:13 +0200

    Add pass-through

---
+++ WebApi/PhotoController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotoController : ControllerBase
+    {
+        [HttpGet("image")]
+        public IActionResult GetImage([FromBody] CoreApp.Photo photo)
+        {
+            if (photo == null || photo.ImageData == null)
+            {
+                return NotFound();
+            }
+            return File(photo.ImageData, "image/jpeg", photo.Id + ".jpg");
+        }
+    }
+}
