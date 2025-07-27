c1ce5589dc4b5fe62dfa51189789e1cfe8205aab
Mateusz <PRS-M@users.noreply.github.com>
2025-07-28 00:01:03 +0200

    Update pass through

--- WebApi/PhotoController.cs
+++ WebApi/PhotoController.cs
@@ -8,13 +8,13 @@ namespace WebApi
     public class PhotoController : ControllerBase
     {
         [HttpGet("image")]
-        public IActionResult GetImage([FromBody] CoreApp.Photo photo)
+        public IActionResult GetImage([FromBody] CoreApp.PhotoWithData photo)
         {
             if (photo == null || photo.ImageData == null)
             {
                 return NotFound();
             }
-            return File(photo.ImageData, "image/jpeg", photo.Id + ".jpg");
+            return File(photo.ImageData, "image/jpeg", photo.FileName + ".jpg");
         }
     }
 }
