23e44e359301d0c190d2b8c0a8f869caa27f6026
Mateusz <PRS-M@users.noreply.github.com>
2025-08-08 21:55:01 +0200

    Add launch config, add a font, navigation layout.

--- MothballMobile/MauiProgram.cs
+++ MothballMobile/MauiProgram.cs
@@ -17,6 +17,7 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("Font Awesome 7 Free-Regular-400.otf", "FontAwesome");
 			});
 
 #if DEBUG
