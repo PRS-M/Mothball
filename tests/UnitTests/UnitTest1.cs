844747ce7c6c2fa49de042823b7d7e3be87ddb77
Mateusz <PRS-M@users.noreply.github.com>
2025-09-08 12:28:54 +0200

    refactor: :art: Restructure and add unit tests project

---
+++ tests/UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace MothballTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
