55aedeeb64273e98892a8918e987316567d0c530
Mateusz <PRS-M@users.noreply.github.com>
2026-08-12 23:26:21 +0200

    feat: :sparkles: Add image counter functionality to ImageCarousel and update usage in Container and Item details pages

--- src/MothballMobile/UI/Controls/ImageCarousel.xaml.cs
+++ src/MothballMobile/UI/Controls/ImageCarousel.xaml.cs
@@ -1,10 +1,13 @@
 using System.Collections;
+using System.Collections.Specialized;
 using Microsoft.Maui.Controls.Shapes;
 
 namespace MothballMobile.UI.Controls;
 
 public partial class ImageCarousel
 {
+	private INotifyCollectionChanged? observedCollection;
+
 	public ImageCarousel()
 	{
 		InitializeComponent();
@@ -26,6 +29,18 @@ public partial class ImageCarousel
 	public static readonly BindableProperty CornerRadiusProperty =
 		BindableProperty.Create(nameof(CornerRadius), typeof(float), typeof(ImageCarousel), 10f);
 
+	public static readonly BindableProperty ShowCounterProperty =
+		BindableProperty.Create(
+			nameof(ShowCounter),
+			typeof(bool),
+			typeof(ImageCarousel),
+			false,
+			propertyChanged: static (bindable, _, __) =>
+			{
+				if (bindable is ImageCarousel control)
+					control.UpdateCounter();
+			});
+
 	public IEnumerable? ImagePaths
 	{
 		get => (IEnumerable?)GetValue(ImagePathsProperty);
@@ -38,6 +53,12 @@ public partial class ImageCarousel
 		set => SetValue(CornerRadiusProperty, value);
 	}
 
+	public bool ShowCounter
+	{
+		get => (bool)GetValue(ShowCounterProperty);
+		set => SetValue(ShowCounterProperty, value);
+	}
+
 	protected override void OnPropertyChanged(string? propertyName = null)
 	{
 		base.OnPropertyChanged(propertyName);
@@ -48,9 +69,74 @@ public partial class ImageCarousel
 
 	void ApplyToCarousel()
 	{
+		DetachObservedCollection();
 		Carousel.ItemsSource = ImagePaths;
-		Carousel.HeightRequest = HeightRequest;
-		Carousel.WidthRequest = WidthRequest;
+		AttachObservedCollection();
+		UpdateCounter();
+	}
+
+	void Carousel_OnPositionChanged(object? sender, PositionChangedEventArgs e)
+	{
+		UpdateCounter();
+	}
+
+	void AttachObservedCollection()
+	{
+		if (ImagePaths is INotifyCollectionChanged ncc)
+		{
+			observedCollection = ncc;
+			observedCollection.CollectionChanged += OnImagePathsCollectionChanged;
+		}
+	}
+
+	void DetachObservedCollection()
+	{
+		if (observedCollection is null)
+			return;
+
+		observedCollection.CollectionChanged -= OnImagePathsCollectionChanged;
+		observedCollection = null;
+	}
+
+	void OnImagePathsCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+	{
+		UpdateCounter();
+	}
+
+	void UpdateCounter()
+	{
+		var total = CountImages(ImagePaths);
+
+		if (!ShowCounter || total <= 1)
+		{
+			CounterLabel.IsVisible = false;
+			CounterLabel.Text = string.Empty;
+			return;
+		}
+
+		var position = Carousel.Position;
+		if (position < 0 || position >= total)
+		{
+			position = 0;
+			Carousel.Position = 0;
+		}
+
+		CounterLabel.IsVisible = true;
+		CounterLabel.Text = $"{position + 1}/{total}";
+	}
+
+	static int CountImages(IEnumerable? source)
+	{
+		if (source is null)
+			return 0;
+
+		var count = 0;
+		foreach (var _ in source)
+		{
+			count++;
+		}
+
+		return count;
 	}
 
 	void Image_OnSizeChanged(object? sender, EventArgs e)
@@ -66,5 +152,15 @@ public partial class ImageCarousel
 			CornerRadius = CornerRadius,
 			Rect = new Rect(0, 0, image.Width, image.Height)
 		};
+	}
+
+	protected override void OnHandlerChanging(HandlerChangingEventArgs args)
+	{
+		if (args.NewHandler is null)
+		{
+			DetachObservedCollection();
+		}
+
+		base.OnHandlerChanging(args);
 	}
 }
