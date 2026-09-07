6ac159d84762725dd75dc4ce08c9d86b748d29fa
Mateusz <PRS-M@users.noreply.github.com>
2026-09-07 23:51:55 +0200

    feat: :lipstick: add platform-native navigation styling

--- src/MothballMobile/MainPage.xaml.cs
+++ src/MothballMobile/MainPage.xaml.cs
@@ -9,21 +9,37 @@ public partial class MainPage : BasePage
 
 	private async void OnContainersClicked(object? sender, EventArgs e)
 	{
+#if IOS || MACCATALYST
+		await Shell.Current.GoToAsync("//MainTabs/Containers/ContainersPage");
+#else
 		await Shell.Current.GoToAsync(Infrastructure.NavigationRoutes.HomeContainers);
+#endif
 	}
 
 	private async void OnItemsClicked(object? sender, EventArgs e)
 	{
+#if IOS || MACCATALYST
+		await Shell.Current.GoToAsync("//MainTabs/Items/ItemsPage");
+#else
 		await Shell.Current.GoToAsync(Infrastructure.NavigationRoutes.HomeItems);
+#endif
 	}
 
 	private async void OnSettingsClicked(object? sender, EventArgs e)
 	{
+#if IOS || MACCATALYST
+		await Shell.Current.GoToAsync("//MainTabs/Settings/SettingsPage");
+#else
 		await Shell.Current.GoToAsync(Infrastructure.NavigationRoutes.Settings);
+#endif
 	}
 
 	private async void OnTagsClicked(object? sender, EventArgs e)
 	{
+#if IOS || MACCATALYST
+		await Shell.Current.GoToAsync("//MainTabs/Tags/TagsPage");
+#else
 		await Shell.Current.GoToAsync(Infrastructure.NavigationRoutes.Tags);
+#endif
 	}
 }
---
+++ src/MothballMobile/Resources/Styles/AppleStyles.xaml.cs
@@ -0,0 +1,9 @@
+namespace MothballMobile.Resources.Styles;
+
+public partial class AppleStyles : ResourceDictionary
+{
+    public AppleStyles()
+    {
+        InitializeComponent();
+    }
+}
